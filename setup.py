b2f9870cf6b9e29b48349a154a8f3b9b14866f86
Scott Stevenson <scott@stevenson.io>
2018-01-14 18:26:08 +0000

    Bump version to 3.0.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read_long_description():
 
 setup(
     name='xdg',
-    version='2.0.0',
+    version='3.0.0',
     description='Variables defined by the XDG Base Directory Specification',
     long_description=read_long_description(),
     url='https://github.com/srstevenson/xdg',
